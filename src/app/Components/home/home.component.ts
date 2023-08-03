b17624a5527257ea37d52bfe0a5f7858ed48531a
jonmoyano <102822376+jonmoyano@users.noreply.github.com>
2023-08-02 21:43:28 -0500

    ...
    ...

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Animal } from 'src/app/Models/animal';
+import { TipoAnimal } from 'src/app/Models/tipoAnimal';
+import { AnimalService } from 'src/app/Service/animal.service';
 import { CargarScrpitsService } from 'src/app/Service/cargar-scrpits.service';
 
 @Component({
@@ -6,14 +9,30 @@ import { CargarScrpitsService } from 'src/app/Service/cargar-scrpits.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
+
+  public animal = new Animal();
+  public listAnimal: Animal[] = [];
+
+
+  public tipoAnimal = new TipoAnimal();
+	public listTipoAnimal: TipoAnimal[] = [];
 
     constructor(
-    private _CargarScript: CargarScrpitsService
+    private _CargarScript: CargarScrpitsService,
+    private animalService: AnimalService
+    
     
   ) {
     _CargarScript.Cargar(["home"]);
   }
+
+  ngOnInit(): void {
+
+    
+	}
+
+
 }
 
 
