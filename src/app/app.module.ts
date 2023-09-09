432a124086ad59b84848fd41b314835c6f055655
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-09-09 17:24:12 -0500

    creacion footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,6 +32,7 @@ import { NgxUiLoaderModule } from 'ngx-ui-loader';
 import { LayoutComponent } from './Components/layout/layout.component';
 import { InformacionComponent } from './Components/informacion/informacion/informacion.component';
 import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 
 
@@ -57,6 +58,7 @@ import { HeaderComponent } from './Components/header/header.component';
     ContactoComponent,
     LayoutComponent,
     HeaderComponent,
+    FooterComponent,
 
   ],
   imports: [
