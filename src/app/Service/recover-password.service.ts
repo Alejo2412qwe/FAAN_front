2ac72907d59887dce30c1aed054bf13e94b2dd81
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-09-09 17:42:30 -0500

    cambios

--- src/app/Service/recover-password.service.ts
+++ src/app/Service/recover-password.service.ts
@@ -2,8 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Usuario } from '../Models/usuario';
-import { environment } from '../environment/enviroment';
 import { RecoverPassword } from '../Models/recover-password';
+import { environment } from 'src/environment/enviroment';
 
 @Injectable({
   providedIn: 'root'
