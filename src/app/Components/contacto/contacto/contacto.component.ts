4926e7e9632358df0f0ad84c3b55fb28578fbabb
Sebas201964 <PEDRO.CABRERA.EST@TECAZUAY.EDU.EC>
2023-09-13 18:05:13 -0500

    correo/contactos

--- src/app/Components/contacto/contacto/contacto.component.ts
+++ src/app/Components/contacto/contacto/contacto.component.ts
@@ -6,6 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
-
+  
+  enviarCorreo() {
+    const nombreInput = document.getElementById('nombre') as HTMLInputElement;
+    const emailInput = document.getElementById('email') as HTMLInputElement;
+    const mensajeTextarea = document.getElementById('mensaje') as HTMLTextAreaElement;
+  
+    const nombre = nombreInput.value;
+    const email = emailInput.value;
+    const mensaje = mensajeTextarea.value;
+  
+    const asunto = 'Mensaje de contacto de ' + nombre;
+    const cuerpo = 'Nombre: ' + nombre + '\n' +
+      'Email: ' + email + '\n' +
+      'Mensaje: ' + mensaje;
+  
+    const mailtoURL = 'mailto:cabrerapedro32@gmail.com' +
+      '?subject=' + encodeURIComponent(asunto) +
+      '&body=' + encodeURIComponent(cuerpo);
+  
+    window.location.href = mailtoURL;
+  
+    // Limpiar los campos después de enviar el correo
+    nombreInput.value = '';
+    emailInput.value = '';
+    mensajeTextarea.value = '';
+  }
+  
 }
+
  
