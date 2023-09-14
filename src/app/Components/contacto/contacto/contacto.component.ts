aae1bba01cd3f18f399d12e3a65285e76978b729
Sebas201964 <PEDRO.CABRERA.EST@TECAZUAY.EDU.EC>
2023-09-13 20:12:35 -0500

    validacionc

--- src/app/Components/contacto/contacto/contacto.component.ts
+++ src/app/Components/contacto/contacto/contacto.component.ts
@@ -6,33 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./contacto.component.css']
 })
 export class ContactoComponent {
-  
+
   enviarCorreo() {
     const nombreInput = document.getElementById('nombre') as HTMLInputElement;
     const emailInput = document.getElementById('email') as HTMLInputElement;
     const mensajeTextarea = document.getElementById('mensaje') as HTMLTextAreaElement;
-  
-    const nombre = nombreInput.value;
-    const email = emailInput.value;
-    const mensaje = mensajeTextarea.value;
-  
+
+    const nombre = nombreInput.value.trim(); // Elimina espacios en blanco al principio y al final
+    const email = emailInput.value.trim();
+    const mensaje = mensajeTextarea.value.trim();
+
+    // Verificar que los campos no estén vacíos
+    if (nombre === '' || email === '' || mensaje === '') {
+      alert('Por favor, complete todos los campos antes de enviar el correo.');
+      return; // Detener la ejecución si hay campos vacíos
+    }
+
     const asunto = 'Mensaje de contacto de ' + nombre;
     const cuerpo = 'Nombre: ' + nombre + '\n' +
       'Email: ' + email + '\n' +
       'Mensaje: ' + mensaje;
-  
+
     const mailtoURL = 'mailto:cabrerapedro32@gmail.com' +
       '?subject=' + encodeURIComponent(asunto) +
       '&body=' + encodeURIComponent(cuerpo);
-  
+
     window.location.href = mailtoURL;
-  
+
     // Limpiar los campos después de enviar el correo
     nombreInput.value = '';
     emailInput.value = '';
     mensajeTextarea.value = '';
   }
-  
+
+
 }
 
- 
